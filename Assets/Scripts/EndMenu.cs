7b465ccd9136eabcb1156d8dd87999d7d20ca33b
ChanHW24 <chanhongwei24@gmail.com>
2024-08-11 15:53:28 +0800

    Update 1.9
    Major Update:
    
    - Added Light probe to significantly
    
    - Arrange item to the rightful place
    
    - End Scene Added
    
    - Navigation button linked to the rightful scene

--- Assets/Scripts/EndMenu.cs
+++ Assets/Scripts/EndMenu.cs
@@ -5,6 +5,20 @@ using UnityEngine.SceneManagement;
 
 public class EndMenu : MonoBehaviour
 {
+    public AudioSource endSound;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = true; // Show cursor
+        Cursor.lockState = CursorLockMode.None; // Unlock cursor
+        endSound.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 
     public void MainMenu()
     {
